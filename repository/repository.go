d9383bc7ca5e908dd5ab1f008cd739b4d56b8237
Zmey56 <zmey56@gmail.com>
2023-08-13 12:56:10 +0400

    change go.mod

--- repository/repository.go
+++ repository/repository.go
@@ -5,4 +5,5 @@ type DatabaseRepo interface {
 	CreateTableSellTechLCC() error
 	InsertDataTable(uid, first_name, last_name string) error
 	CountData() (int, error)
+	GetNameFromDB(names []string, nameType string) ([]SDN, error)
 }
